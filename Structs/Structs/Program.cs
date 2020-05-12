95581d305930280e5664c74f5b5e5700d8fe1786
gjmiii9@gmail.com <gjmiii9@gmail.com>
2020-05-12 11:34:30 -0700

    structs init commit

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Structs
+{
+    class Program
+    {
+        struct Game
+        {
+            public string name;
+            public string developer;
+            public double rating;
+            public string releaseDate;
+
+            public void Display()
+            {
+                Console.WriteLine("Game 1's name is {0}", name);
+                Console.WriteLine("Game 1's developer is {0}", developer);
+                Console.WriteLine("Game 1's rating is {0}", rating);
+                Console.WriteLine("Game 1's release date is {0}", releaseDate);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Game game1;
+
+            game1.name = "Dragamon Go";
+            game1.developer = "Joe";
+            game1.rating = 3.5;
+            game1.releaseDate = "1/7/2019";
+
+
+            game1.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
