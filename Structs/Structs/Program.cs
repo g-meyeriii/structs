7623da9621a513844c41f14bf16988e37b25c6c9
gjmiii9@gmail.com <gjmiii9@gmail.com>
2020-05-12 12:39:28 -0700

    math added

--- Structs/Structs/Program.cs
+++ Structs/Structs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Structs
 {
@@ -30,26 +31,28 @@ namespace Structs
         //    }
         //}
 
-            enum Day { Mo,Tu,We,Th,Fr,Sa,Su}
+
+            ////Enums can be used to share a set of constants, ex place in the namespace and can be shared across all the classes in that name space;
+            //enum Day { Mo,Tu,We,Th,Fr,Sa,Su}
             
-            enum Month { Jan=1,Feb,Mar,Apr,May,Jun,Jul=12,Aug,Sep,Oct,Nov,Dec};
+            //enum Month { Jan=1,Feb,Mar,Apr,May,Jun,Jul=12,Aug,Sep,Oct,Nov,Dec};
 
         static void Main(string[] args)
         {
-            Day fr = Day.Fr;
-            Day su = Day.Su;
+            //Day fr = Day.Fr;
+            //Day su = Day.Su;
 
-            Day a = Day.Fr;
+            //Day a = Day.Fr;
 
 
 
-            Console.WriteLine(fr== a);
+            //Console.WriteLine(fr== a);
 
-            Console.WriteLine(Day.Mo);
-            Console.WriteLine((int)Day.Mo);
+            //Console.WriteLine(Day.Mo);
+            //Console.WriteLine((int)Day.Mo);
 
-            Console.WriteLine((int)Month.Feb);
-            Console.WriteLine((int)Month.Aug);
+            //Console.WriteLine((int)Month.Feb);
+            //Console.WriteLine((int)Month.Aug);
             //Game game1;
 
             //game1.Display();
@@ -62,7 +65,23 @@ namespace Structs
 
             //game1.Display();
 
-            Console.ReadKey();
+            Console.WriteLine("Ceiling: " + Math.Ceiling(15.3));
+            
+            Console.WriteLine("Floor: " + Math.Floor(15.3));
+           
+
+            int num1 = 13;
+            int num2 = 9;
+            Console.WriteLine("Lower of num1 {0} and num2 {1} is {2}", num1, num2, Math.Min(num1, num2 ));
+            Console.WriteLine("Higher of num1 {0} and num2 {1} is {2}", num1, num2, Math.Max(num1, num2));
+
+            Console.WriteLine("3 to the power of 5 is {0}", Math.Pow(3,5));
+            Console.WriteLine("Pi is: {0}", Math.PI);
+            Console.WriteLine("The square root of 25 is {0}", Math.Sqrt(25));
+            Console.WriteLine("Always positive is {0}", Math.Abs(-25) );
+            Console.WriteLine("cos of 1 is {0}", Math.Cos(1));
+
+            Console.ReadLine();
         }
     }
 }
